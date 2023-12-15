80e8b49edc4e67a2fb926ca953055d4c8f38cf8e
Juliano Sena da Silva Carlos <juliano.carlos@partner.bmw.de>
2023-12-15 13:17:45 +0000

    Interview for hostfully

---
+++ hostfully/src/test/groovy/com/interview/application/usecase/CreateBookingUseCaseSpec.groovy
@@ -0,0 +1,17 @@
+package com.interview.application.usecase
+
+import spock.lang.Specification
+
+class CreateBookingUseCaseSpec extends Specification {
+
+    def "Trying to test something"(){
+        given: "Something"
+        def given = "1"
+
+        when: "Another thing"
+        def when = "1"
+
+        then: "One is equal 1"
+        1 == 1
+    }
+}
---
+++ hostfully/src/test/java/com/interview/application/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.interview.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
