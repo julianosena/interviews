db0a884f971d3defbf58b8c1b2c98320f31f579e
Juliano Sena da Silva Carlos <juliano.carlos@partner.bmw.de>
2023-12-17 16:57:34 +0000

    Creating the test for creating reservation

--- hostfully/src/test/groovy/com/interview/application/usecase/CreateReservationUseCaseSpec.groovy
+++ hostfully/src/test/groovy/com/interview/application/usecase/CreateReservationUseCaseSpec.groovy
@@ -4,9 +4,12 @@ import com.interview.application.domain.Hotel
 import com.interview.application.domain.Reservation
 import com.interview.application.domain.fixture.*
 import com.interview.application.gateway.CreateReservationGateway
+import com.interview.application.usecase.exception.UseCaseException
 import spock.lang.Specification
 
 import java.time.LocalDate
+
+import static com.interview.application.domain.Reservation.Status.PENDING;
 
 class CreateReservationUseCaseSpec extends Specification {
 
@@ -67,5 +70,56 @@ class CreateReservationUseCaseSpec extends Specification {
         null != reserved
         and : "It must carry id within"
         null != reserved.id
+        and : "its status must be as expected"
+        reservation.status == PENDING
+    }
+
+    def "It should throw an exception because there is no availability for the room in a specific date"(){
+        given: "Valid and existent hotel"
+        Hotel hotel = HotelFixture.create()
+        and : "its rooms."
+        def rooms = RoomFixture.list(hotel)
+        hotel.rooms = rooms
+
+        and : "The booker would like to make a reservation"
+        def booker = BookerFixture.create(id : null)
+        and : "than, It selects a room from hotel"
+        def selectedRoom = rooms.findAll { room -> room.type.name == "Standard" }.getFirst()
+
+        and : "select the period It would like to stay there"
+        def checkinDate = LocalDate.now()
+        def checkoutDate = checkinDate.plusMonths(1)
+
+        and : "than, It submits a reservation"
+        def roomReservation = RoomReservationFixture.create(id : null, room : selectedRoom)
+        def reservation = ReservationFixture.create([
+                id : null,
+                booker : booker,
+                checkinDate: checkinDate,
+                checkoutDate: checkoutDate,
+                numberOfAdults: 2,
+                numberOfChildren: 1,
+                roomReservations: [roomReservation]
+        ])
+
+        when : "use case is called"
+        useCase.execute(reservation)
+
+        then : "checking availability process should return that there is availability for this reservation"
+        1 * isThereAvailabilityForTheReservationUseCase.execute(reservation) >> {
+            false
+        }
+
+        and : "the process of creation reservation should not be executed"
+        0 * createReservationGateway.execute(reservation)
+
+        and : "the use case should throw an expcetion"
+        def e = thrown(UseCaseException)
+        and : "exception should not be null"
+        null != e
+        and : "its message should be the expected"
+        e.message == "There is no availability for this period and " +
+                "room(s) " + reservation.getRoomReservations() + " " +
+                reservation.getCheckinDate() + " up to " + reservation.getCheckoutDate()
     }
 }
